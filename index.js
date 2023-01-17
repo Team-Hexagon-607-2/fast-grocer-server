827a1d01386d85d3ee2e8f31eae45a403106a2fa
atiqulislamwb <atiqulislamrussell0@gmail.com>
2023-01-17 21:53:01 +0600

    contain search route

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
-
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const fuzzy = require("fuzzysearch");
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -11,54 +11,66 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ifxvjm9.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-    try{
-        const productsCollection = client.db('fastGrocer').collection('products');
-        const categoriesCollection = client.db('fastGrocer').collection('categories');
+async function run() {
+  try {
+    const productsCollection = client.db("fastGrocer").collection("products");
+    const categoriesCollection = client
+      .db("fastGrocer")
+      .collection("categories");
 
-        app.post('/products', async(req, res) => {
-            const product = req.body;
-            const result = await productsCollection.insertOne(product);
-            res.send(result);
-        });
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await productsCollection.insertOne(product);
+      res.send(result);
+    });
 
-        app.get('/products', async(req, res) => {
-            const query = {};
-            const products = await productsCollection.find(query).toArray();
-            res.send(products);
-        })
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const products = await productsCollection.find(query).toArray();
+      res.send(products);
+    });
 
-        app.get('/product/:id', async(req, res) =>{
-            
-        })
+    app.get("/product/:id", async (req, res) => {});
 
-        app.post('/categories', async(req, res) => {
-            const category = req.body;
-            const result = await categoriesCollection.insertOne(category);
-            res.send(result);
-        })
+    app.post("/categories", async (req, res) => {
+      const category = req.body;
+      const result = await categoriesCollection.insertOne(category);
+      res.send(result);
+    });
 
-        app.get('/categories', async(req, res) => {
-            const query = {};
-            const categories = await categoriesCollection.find(query).toArray();
-            res.send(categories);
-        })
-    }
-    finally{
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const categories = await categoriesCollection.find(query).toArray();
+      res.send(categories);
+    });
 
-    }
+    //This route (search route) made by atiqul islam
+
+    app.get("/search", async (req, res) => {
+      const searchText = req.query.q;
+      console.log(searchText);
+      const query = { $text: { $search: searchText } };
+
+      const matches = await productsCollection.find(query).toArray();
+
+      res.send(matches);
+    });
+  } finally {
+  }
 }
 
-run().catch(console.log)
+run().catch(console.log);
 
-app.get('/', async(req, res) =>{
-    res.send('Fast Grocer server is running');
-})
+app.get("/", async (req, res) => {
+  res.send("Fast Grocer server is running");
+});
 
 app.listen(port, () => console.log(`Fast Grocer running on ${port}`));
