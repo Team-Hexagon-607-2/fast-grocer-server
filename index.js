ea4e6af4cba724f3460aa71472edfe9bb6bddab6
saruaralam-swd <saruaralam2001@gmail.com>
2023-01-19 12:26:02 +0600

    product category add

--- index.js
+++ index.js
@@ -16,38 +16,38 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ifxvjm9.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         const productsCollection = client.db('fastGrocer').collection('products');
         const categoriesCollection = client.db('fastGrocer').collection('productCategory');
         const usersCollection = client.db('fastGrocer').collection('users');
 
-        app.post('/products', async(req, res) => {
+        app.post('/products', async (req, res) => {
             const product = req.body;
             const result = await productsCollection.insertOne(product);
             res.send(result);
         });
 
-        app.get('/products', async(req, res) => {
+        app.get('/products', async (req, res) => {
             const query = {};
             const products = await productsCollection.find(query).toArray();
             res.send(products);
         })
 
-        app.get('/products/:id', async(req, res) =>{
+        app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const result = await productsCollection.findOne(query);
             res.send(result);
         })
 
-        app.post('/categories', async(req, res) => {
+        app.post('/categories', async (req, res) => {
             const category = req.body;
             const result = await categoriesCollection.insertOne(category);
             res.send(result);
         })
 
-        app.get('/categories', async(req, res) => {
+        app.get('/categories', async (req, res) => {
             const query = {};
             const categories = await categoriesCollection.find(query).toArray();
             res.send(categories);
@@ -55,7 +55,7 @@ async function run(){
 
         app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const product = await productsCollection.findOne(query);
             res.send(product);
         })
@@ -66,21 +66,28 @@ async function run(){
             res.send(result);
         })
 
-        app.get('/users', async(req, res) => {
+        app.get('/users', async (req, res) => {
             const query = {};
             const users = await usersCollection.find(query).toArray();
             res.send(users);
         })
 
+        app.get("/search", async (req, res) => {
+            const searchText = req.query.q;
+            const query = { $text: { $search: searchText } };
+            const matches = await productsCollection.find(query).toArray();
+            res.send(matches);
+        });
+
     }
-    finally{
+    finally {
 
     }
 }
 
 run().catch(console.log)
 
-app.get('/', async(req, res) =>{
+app.get('/', async (req, res) => {
     res.send('Fast Grocer server is running');
 })
 
