d1f592f9018aeb5a1a327cf3ca2d5c9e3f20d974
saruaralam-swd <saruaralam2001@gmail.com>
2023-02-11 12:11:01 +0600

    product delete api add done

--- index.js
+++ index.js
@@ -138,6 +138,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/product-delete/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.get("/products", async (req, res) => {
       const query = {};
       const products = await productsCollection.find(query).toArray();
@@ -432,15 +439,15 @@ async function run() {
 
     app.get("/users/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: ObjectId(id) };
+      const query = { _id: ObjectId (id) };
       const user = await usersCollection.findOne(query);
       res.send(user);
     });
 
     app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = { _id: ObjectId(id) };
-      const result = await usersCollection.deleteOne(filter);
+      const query = {_id: ObjectId(id)};
+      const result = await productsCollection.deleteOne(query);
       res.send(result);
     });
 
