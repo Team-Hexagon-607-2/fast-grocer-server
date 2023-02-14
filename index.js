1ea38ab374b244c949498cae503edfa8af5302bb
saruaralam-swd <saruaralam2001@gmail.com>
2023-02-14 23:38:44 +0600

    products/:id api delete

--- index.js
+++ index.js
@@ -237,13 +237,6 @@ async function run() {
       res.send(categories);
     });
 
-    // app.get("/products/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: ObjectId(id) };
-    //   const product = await productsCollection.findOne(query);
-    //   res.send(product);
-    // });
-
     app.post("/reviews", async (req, res) => {
       const review = req.body;
       const result = await reviewsCollection.insertOne(review);
